36bab50e582a3c056b32ee1af4bfe18772c43eef
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2022-07-13 15:31:24 +0800

    Update Major Activity 3.0 - C Strings.c

--- Major Activity 3.0 - C Strings.c
+++ Major Activity 3.0 - C Strings.c
@@ -15,30 +15,25 @@ int main()
   	printf("Reverse ordered words:\n");
   	for(i = len-1 ; i >= 0; i--)
 	{
-		if(str[i] == ' ' || i == '\0')
-		{
-			if(i=='\0')
-			{
-       startIndex = 0;
+	  if(str[i] == ' ' || i == '\0')
+	  {
+	    if(i=='\0')
+	    {
+              startIndex = 0;
+            }
 
-			}
-
-			else
-    {
-       startIndex=i+1;
-
-    }
-
-	for(j = startIndex; j <= endIndex; j++)
-      {
-	printf("%c",str[j]);
-
-      }
+	 else
+            {
+              startIndex=i+1;
+            }
+        for(j = startIndex; j <= endIndex; j++)
+        {
+	 printf("%c",str[j]);
+        }
 	 endIndex = i - 1;
          printf(" ");
-		}
-	}
-
+        }
+      }
   	return 0;
 }
     
